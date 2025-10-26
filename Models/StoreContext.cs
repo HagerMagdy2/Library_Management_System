2e4e5a4c3fd44d4edfe013e79f23230d64c6566d
HagerMagdy2 <hager9671@gmail.com>
2025-10-26 23:52:10 +0300

    Add project files.

---
+++ Models/StoreContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library_Management_System.Models
+{
+    public class StoreContext:IdentityDbContext
+    {
+        public StoreContext()
+        {
+            
+        }
+        public StoreContext(DbContextOptions<StoreContext> op):base(op) 
+        {
+            
+        }
+    }
+}
